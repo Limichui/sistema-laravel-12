e0f15b2e0b462ffbff4b321d5e9bb844c9a22f6b
Limichui <limbert.olmos.m@gmail.com>
2025-07-02 22:55:18 -0400

    Rutas y Helpers

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,59 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Definición básica de rutas
+Route::get('/hola', function(){
+    $nombre="Limbert Olmos";
+    return "Hola, Laravel {$nombre}";
+});
+
+//Parámetros en las rutas
+//Parámetros Requeridos
+Route::get('/usuario/{nombre}', function($nombre){
+    return "Usuario: $nombre";
+});
+
+//Parámetros con valores por defecto
+Route::get('/usuario/{nombre?}', function($nombre='Default'){
+    return "Usuario: $nombre";
+});
+
+//Rutas nombradas
+Route::get('/perfil', function(){
+    return view('perfil');
+})->name('perfil');
+
+//Redirección de rutas
+Route::redirect('/ruta-anterior','/ruta-nueva');
+
+Route::get('/ruta-nueva', function(){
+    return "Ruta nueva";
+});
+
+//Vista directa
+Route::view('/bienvenido','welcome');
+
+//Grupos de ruta
+//Agrupación con prefijos
+Route::group(['prefix'=>'admin'], function(){
+    Route::get('/dashboard', function(){
+        return "Admin dashboard";
+    });
+});
+
+//Helpers
+//env(); Obtener variables de entorno
+Route::get('/db', function () {
+    return env('DB_CONNECTION');
+});
+
+//dd(); Depuración de variables
+Route::get('/dd', function () {
+    $data = ['name' => 'Limbert Olmos', 'role' => 'Developer'];
+    dd($data); // Detiene la ejecución y muestra el contenido de $data
+});
+
+//config(); Obtener configuración de la aplicación
+Route::get('/config', function () {
+    return config('app.timezone'); // Devuelve el nombre de la aplicación configurado en config/app.php
+});
